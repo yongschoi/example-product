38a88b61715661006fa511a07dfe01f9c11f4db6
yongschoi <yongschoi@naver.com>
2022-01-07 18:44:51 +0900

    Gateway 방식 적용

--- src/main/java/yongs/temp/ExampleProductApplication.java
+++ src/main/java/yongs/temp/ExampleProductApplication.java
@@ -2,12 +2,18 @@ package yongs.temp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ExampleProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleProductApplication.class, args);
 	}
-
+	@GetMapping("/")
+    public String home() throws Exception {
+        return "Hello, example-product";
+    }
 }
